59625581a72375ccb2d88ee41501c5d3b7de04f5
Crystal Ng <ng.crystal.wy@gmail.com>
2019-04-28 01:00:58 +0000

    Added test code to secure edit, update, destroy grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
   end
@@ -15,11 +15,48 @@ class GramsController < ApplicationController
     else
       redirect_to root_path
     end
+  end
+  
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_status(:not_found) if @gram.blank?
+  end
+  
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_status(:not_found) if @gram.blank?
+    return render_status(:forbidden) if @gram.user != current_user
+  end
+  
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_status(:not_found) if @gram.blank?
+    return render_status(:forbidden) if @gram.user != current_user
     
+    @gram.update_attributes(gram_params)
+    
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    @gram = Gram.find_by_id(params[:id])
+    return render_status(:not_found) if @gram.blank?
+    return render_status(:forbidden) if @gram.user != current_user
+    
+    @gram.destroy
+    redirect_to root_path
   end
   
   private
   def gram_params
     params.require(:gram).permit(:message, :user)
+  end
+  
+  def render_status(status)
+    render plain: "#{status.to_s.titleize}", status: status
   end
 end
